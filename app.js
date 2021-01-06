aab37fb62983c5271c8d313ea581a7c91d3284bb
joaowilian01 <joaowilian01@gmail.com>
2021-01-06 20:31:51 -0300

    Refactored code to improve readability

--- app.js
+++ app.js
@@ -1,36 +1,58 @@
 const correctAnswers = ['A', 'A', 'B', 'A']
 
+let score = 0
+
 const form = document.querySelector('.quiz-form')
-const result = document.querySelector('.result')
+const finalScoreContainer = document.querySelector('.final-score-container')
 
-form.addEventListener('submit', event => {
-  event.preventDefault()
-  
-  let score = 0
-  let counter = 0
+const getUserAnswers = () => {
+  let userAnswers = []
 
-  const userAnswers = [
-    form.inputQuestion1.value,
-    form.inputQuestion2.value,
-    form.inputQuestion3.value,
-    form.inputQuestion4.value
-  ]
+  correctAnswers.forEach((_, index) => {
+    const userAnswer = form[`inputQuestion${index+1}`].value
+    userAnswers.push(userAnswer)
+  })
 
+  return userAnswers
+}
+
+const calculateUserScore = userAnswers => {
   userAnswers.forEach((userAnswer, index) => {
-    if (userAnswer === correctAnswers[index]) {
+    const isUserAnswerCorrect = userAnswer === correctAnswers[index]
+    
+    if (isUserAnswerCorrect) {
       score += 25
     }
   })
+}
 
-  scrollTo(0, 0)
+const showFinalScore = () => {
+  scrollTo({
+    top: 0,
+    left: 0,
+    behavior: 'smooth'
+  })
+  finalScoreContainer.classList.remove('d-none')
+}
 
-  result.classList.remove('d-none')
+const animateFinalScore = () => {
+  let counter = 0
 
   const timer = setInterval(() => {
     if (counter === score) {
       clearInterval(timer)
     }
-    result.querySelector('span').textContent = `${counter}%`
-    counter++
+    finalScoreContainer.querySelector('span').textContent = `${counter++}%`
+    
   }, 10);
+}
+
+form.addEventListener('submit', event => {
+  event.preventDefault()
+  
+  const userAnswers = getUserAnswers()
+
+  calculateUserScore(userAnswers)
+  showFinalScore()
+  animateFinalScore()
 })
